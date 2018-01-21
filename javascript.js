0b2ec5e61dcde9ab92d4b27ce3d21a1a7da901bc
Braga <braga@MacBook-Pro-2.local>
2018-01-21 19:41:25 -0200

    placar funcionando

--- javascript.js
+++ javascript.js
@@ -10,14 +10,32 @@ function addBola() {
 	var c3 = Math.floor(Math.random() * 10);
 	bola.setAttribute("style", "left: "+p1+"px;top: "+p2+"px; background-color: #"+c1+""+c2+""+c3+" ;");
 	bola.setAttribute("onClick", "estourar(this)");
+	
 
 	document.body.appendChild(bola);
 
 }
+
+var x = 0;
+
 function estourar(elemento){
+	
 	document.body.removeChild(elemento);
+
+	var ponto = document.getElementById("pontos");
+	
+	x = x + Math.floor(Math.random()*10);
+
+	y = x
+
+	document.getElementById('pontos').innerHTML = y;  
+
+	y = document.getElementById('pontos').innerHTML;
+
 }
 
 function iniciar(){
-	setInterval(addBola, 1000);
+	setInterval(addBola, 0850);
 }
+
+
