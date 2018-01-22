25ca444554ea1076c6d4f093963f09122b3a0430
Braga <braga@MacBook-Pro-2.local>
2018-01-21 22:57:24 -0200

    algumas alteracoes

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ function addBola() {
 	var c1 = Math.floor(Math.random() * 10);
 	var c2 = Math.floor(Math.random() * 10);
 	var c3 = Math.floor(Math.random() * 10);
-	bola.setAttribute("style", "left: "+p1+"px;top: "+p2+"px; background-color: #"+c1+""+c2+""+c3+" ;");
+	bola.setAttribute("style", "left: "+p1+"px;top: "+p2+"px; background-color: #"+c1+""+c2+""+c3+";transition: 0.5s;");
 	bola.setAttribute("onClick", "estourar(this)");
 	
 
@@ -19,7 +19,7 @@ function addBola() {
 var x = 0;
 
 function estourar(elemento){
-	
+
 	document.body.removeChild(elemento);
 
 	var ponto = document.getElementById("pontos");
@@ -28,14 +28,25 @@ function estourar(elemento){
 
 	y = x
 
-	document.getElementById('pontos').innerHTML = y;  
+	document.getElementById('pontos').innerHTML = y;
 
 	y = document.getElementById('pontos').innerHTML;
+
+	if (y >= 100 && y<= 110) {
+		var resp;
+		alert("Parbéns, você desperdiçou um tempinho da sua vida testando esse jogo! Muito Obrigado.");
+		resp = prompt("Deseja continuar jogando ? sim ou nao");
+		if (resp === "sim") {
+			alert("Okay! pode continuar")
+		} else {
+			location.reload();
+		}
+	}  
 
 }
 
 function iniciar(){
-	setInterval(addBola, 0850);
+	setInterval(addBola, 1000);
 }
 
 
